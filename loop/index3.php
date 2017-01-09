970965b665474839abb1a19b845bd6c12e0c09d7
simplon <rudy__10@hotmail.fr>
2017-01-09 11:08:22 +0100

    exo3

---
+++ loop/index3.php
@@ -0,0 +1,19 @@
+<?php 
+
+$first = 100;
+$second = 10;
+$multip = $first * $second;
+$decr = $first --;
+
+while ( $first >= 10) {
+	
+	echo  "<li>$multip</li>";
+	
+
+	echo $first--;
+
+}
+
+
+
+ ?>
