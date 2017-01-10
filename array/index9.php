99e7d313719a6c8bd3fe712dc401deb578a5ace8
simplon <rudy__10@hotmail.fr>
2017-01-10 11:31:07 +0100

    exo 9

---
+++ array/index9.php
@@ -0,0 +1,18 @@
+<?php 
+
+$departement= array(02=>"aisne",59=>"nord",60=>"oise",62=>"pas-de-calais",80=>"somme");
+
+$departement[51]= "reims";
+// array_push($departement,"apple");
+
+// print_r($departement);
+
+foreach ($departement as $key => $value) {
+
+
+	echo $value ." ";
+	# code...
+}
+
+
+ ?>
