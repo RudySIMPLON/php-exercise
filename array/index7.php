55ec92834879827cd27fda3fcd11309ea3907c9b
simplon <rudy__10@hotmail.fr>
2017-01-10 11:04:52 +0100

    exo7

---
+++ array/index7.php
@@ -0,0 +1,11 @@
+<?php 
+
+$departement= array(02=>"aisne",59=>"nord",60=>"oise",62=>"pas-de-calais",80=>"somme");
+
+$departement[51]= "reims";
+// array_push($departement,"apple");
+
+print_r($departement);
+ ?>
+
+
