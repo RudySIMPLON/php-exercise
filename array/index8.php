afeab487715d1e82a77b24e5c44deefa947117e7
simplon <rudy__10@hotmail.fr>
2017-01-10 11:29:54 +0100

    correctionexercice

--- array/index8.php
+++ array/index8.php
@@ -1,13 +1,11 @@
 <?php 
 
-$departement= array(02=>"aisne",59=>"nord",60=>"oise",62=>"pas-de-calais",80=>"somme");
-
-$departement[51]= "reims";
+$mois = array("janvier","février","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","décembre");
 // array_push($departement,"apple");
 
 // print_r($departement);
 
-foreach ($departement as $key => $value) {
+foreach ($mois as $key => $value) {
 
 
 	echo $value ." ";
