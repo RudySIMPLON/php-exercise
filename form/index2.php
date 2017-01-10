47eb5c4e6b9ed1b9d0c19546ad29572d2c43a680
simplon <rudy__10@hotmail.fr>
2017-01-10 22:20:58 +0100

    exo 2

---
+++ form/index2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="user2.php" method="GET">
+	<input type="text" name="nom">
+	<input type="text" name="prenom">
+	<input type="submit">
+	
+	
+</form>
+</body>
+</html>
