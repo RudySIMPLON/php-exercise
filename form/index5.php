35a3feb60ccc194aaa57f63fa209f00cca9a6ee7
simplon <rudy__10@hotmail.fr>
2017-01-11 12:10:43 +0100

    exo5

---
+++ form/index5.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="index.php" method="GET">
+<select name="civilité" id="">select 
+	<option value="" name="monsieur">Mr</option>
+	<option value="" name="madame"> Mme</option>
+</select>
+<input type="text" name="prenom">
+<input type="text" name="nom">
+<input type="submit" name="validé">
+	
+
+</form>
+	
+</body>
+</html>
