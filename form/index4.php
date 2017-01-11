1ba141686e9dc9e641c230fb92d380df0e0c9b5f
simplon <rudy__10@hotmail.fr>
2017-01-11 12:09:35 +0100

    exo4

---
+++ form/index4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="user4.php" method="GET">
+	<input type="text" name="nom">
+	<input type="text" name="prenom">
+	<input type="submit" name="validé">
+	
+	
+</form>
+</body>
+</html>
