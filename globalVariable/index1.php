ffb3569f4c24880b0b59f312b5babd639442f67a
simplon <rudy__10@hotmail.fr>
2017-01-12 12:01:28 +0100

    ajout structure html

--- globalVariable/index1.php
+++ globalVariable/index1.php
@@ -1,8 +1,18 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	
 
-echo $_SERVER['HTTP_USER_AGENT']. "<br/>"
-.$_SERVER['REMOTE_ADDR']."<br/>"
-.$_SERVER['SERVER_NAME'];
+	<h1><?= $_SERVER['HTTP_USER_AGENT']; ?></h1>
+	<h2><?= $_SERVER['REMOTE_ADDR'] ;  ?></h2>
+	<h3><?= $_SERVER['SERVER_NAME'];  ?></h3>
+</body>
+</html>
 
 
- ?>
+
+
