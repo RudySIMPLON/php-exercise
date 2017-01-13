bdf663950dfe80eb9bb8056c63f1019aee8720f5
simplon <rudy__10@hotmail.fr>
2017-01-13 12:19:39 +0100

    exo4

--- date/index4.php
+++ date/index4.php
@@ -1,8 +1,11 @@
+<?php 
+// $date=time();
+$mois=8;
+$jours= 02;
+$annees= 2016;
+$date=mktime(0, 0, 0, $mois, $jours, $annees);
 
-<?php
-$date = new DateTime();
-echo $date->getTimestamp();
-?>
+echo $date;
 
 
-
+ ?>
